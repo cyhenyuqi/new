53c00594a1e5558a4778ac933403aa98f0ee5963
cyhenyuqi <1127951214@qq.com>
2020-01-06 15:41:38 +0800

    First commit

---
+++ src/my/bb.java
@@ -0,0 +1,40 @@
+package my;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+public class bb {
+	static int o;
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+	
+		System.out.println(o);
+		 System.out.println("陈禹奇chenyuqi");
+	//	 helloworld.main(args);
+	     helloworld.ccc(3, 33);	
+	     System.out.println();
+	     helloworld ccccc=new helloworld();
+	     ccccc.aaa(3,33);
+	     System.out.println("");
+	     int a=helloworld.qqq(0, 'b');
+	     System.out.println(a);
+	     a=ccccc.qqq(0, 'a');
+	     System.out.println(a);
+	     System.out.println("\n);");
+	//     www.main(args);
+	 //    eee.main(args);
+	     System.out.println("下面开始输入环节");
+	   BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+	   String str=new String();
+	 str=bf.readLine();
+	 
+	   System.out.println();
+	  System.out.println(str);
+	  
+	}
+              
+}
---
+++ src/my/helloworld.java
@@ -0,0 +1,62 @@
+package my;
+
+public class helloworld {
+
+	
+
+/*
+	public static void main(String[] args) {
+         System.out.println("chenyuqi");
+       
+         
+	}
+
+*/	
+	public static  void ccc(int a,int b) {
+		
+		int c;
+		c=a+b;
+		System.out.println(c);
+		System.out.print(b);
+	}
+	
+	public void aaa(int a,int b) {
+	
+		int c;
+		c=a+b;
+		System.out.println(c);
+		System.out.print(b);
+	}
+	
+	public static int qqq(int a,char b) {
+		
+		
+		return b;
+		
+		
+		
+	}
+}
+
+class www extends helloworld{
+	
+	public void aaa(int a,int b) {
+		
+		int c;
+		c=a-b;
+		System.out.println(c);
+		System.out.print(b);
+		
+	}
+	
+class eee extends www{
+	
+	
+	
+	
+}	
+	
+	
+	
+	
+}
